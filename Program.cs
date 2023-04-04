d914387aace973311831d8297e5130f185c57040
unknown <zab_vl@tutby>
2023-04-04 19:25:11 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число:");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] array = new double [rows,columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+      array[i,j]=new Random().NextDouble().;
+      Console.Write(array[i,j]+" ");
+   }
+   Console.WriteLine();
+}
